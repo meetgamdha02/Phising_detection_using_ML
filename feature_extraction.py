29abf85c43cf7559885b9a7c6a4034cd85c0450f
jaydeep11 <jaydeep112000@gmail.com>
2019-11-20 13:41:14 +0530

    Added remaining features

--- feature_extraction.py
+++ feature_extraction.py
@@ -12,7 +12,7 @@ import requests
 import urllib
 import socket
 import ssl
-
+#from googlesearch.googlesearch import GoogleSearch
 #getting raw urls
 #raw_data=pd.read_csv("raw_urls/phising.txt",header=None,names=['urls'])
 raw_data=pd.read_csv("raw_urls/legitimate.txt",header=None,names=['urls'])
@@ -304,6 +304,46 @@ class FeatureExtract:
                     return 0
             # If none of the iframes have a width or height of zero or a frameBorder of size 0, then it is safe to return 1.
             return 1
+    def age_of_domain(self,domain):
+        creation_date = domain.creation_date
+        expiration_date = domain.expiration_date
+        ageofdomain = 0
+        if expiration_date:
+            ageofdomain = abs((expiration_date - creation_date).days)
+        return -1 if ageofdomain / 30 < 6 else 1
+    def web_traffic(self,url):
+        try:
+            rank = \
+                bs4.BeautifulSoup(urllib.request.urlopen("http://data.alexa.com/data?cli=10&dat=s&url=" + url).read(), 'xml').find(
+                    "REACH")['RANK']
+        except TypeError:
+            return -1
+        rank = int(rank)
+        return 1 if rank < 100000 else 0
+    def google_index(self,url):
+        site = GoogleSearch().search(url, 5)
+        return 1 if site else -1
+    def statistical_report(self,url, hostname):
+        try:
+            ip_address = socket.gethostbyname(hostname)
+        except:
+            return -1
+        url_match = re.search(
+            r'at\.ua|usa\.cc|baltazarpresentes\.com\.br|pe\.hu|esy\.es|hol\.es|sweddy\.com|myjino\.ru|96\.lt|ow\.ly', url)
+        ip_match = re.search(
+            '146\.112\.61\.108|213\.174\.157\.151|121\.50\.168\.88|192\.185\.217\.116|78\.46\.211\.158|181\.174\.165\.13|46\.242\.145\.103|121\.50\.168\.40|83\.125\.22\.219|46\.242\.145\.98|'
+            '107\.151\.148\.44|107\.151\.148\.107|64\.70\.19\.203|199\.184\.144\.27|107\.151\.148\.108|107\.151\.148\.109|119\.28\.52\.61|54\.83\.43\.69|52\.69\.166\.231|216\.58\.192\.225|'
+            '118\.184\.25\.86|67\.208\.74\.71|23\.253\.126\.58|104\.239\.157\.210|175\.126\.123\.219|141\.8\.224\.221|10\.10\.10\.10|43\.229\.108\.32|103\.232\.215\.140|69\.172\.201\.153|'
+            '216\.218\.185\.162|54\.225\.104\.146|103\.243\.24\.98|199\.59\.243\.120|31\.170\.160\.61|213\.19\.128\.77|62\.113\.226\.131|208\.100\.26\.234|195\.16\.127\.102|195\.16\.127\.157|'
+            '34\.196\.13\.28|103\.224\.212\.222|172\.217\.4\.225|54\.72\.9\.51|192\.64\.147\.141|198\.200\.56\.183|23\.253\.164\.103|52\.48\.191\.26|52\.214\.197\.72|87\.98\.255\.18|209\.99\.17\.27|'
+            '216\.38\.62\.18|104\.130\.124\.96|47\.89\.58\.141|78\.46\.211\.158|54\.86\.225\.156|54\.82\.156\.19|37\.157\.192\.102|204\.11\.56\.48|110\.34\.231\.42',
+            ip_address)
+        if url_match:
+            return -1
+        elif ip_match:
+            return -1
+        else:
+            return 1
 
 
 #prepare features
@@ -326,6 +366,10 @@ sfh=[]
 submitting_to_email=[]
 abnormal_url=[]
 i_frame=[]
+age_of_domain=[]
+web_traffic=[]
+google_index=[]
+statistical_report=[]
 
 #Extracting features from url
 fe=FeatureExtract()
@@ -367,6 +411,10 @@ for i in range(0,nrows):
     abnormal_url.append(-1 if dns == -1 else fe.abnormal_url(domain, url))
     SSL_final_state.append(-1 if dns == -1 else fe.ssl_final_state(domain,url))
     i_frame.append(fe.i_frame(soup,notfound))
+    age_of_domain.append(-1 if dns == -1 else fe.age_of_domain(domain))
+    web_traffic.append(fe.web_traffic(url))
+    google_index.append(fe.google_index(url))
+    statistical_report.append(fe.statistical_report(url,hostname))
     print(fe.has_ip_address(url))
     print(fe.url_length(url))
     print(fe.having_at_symbol(url))
@@ -385,8 +433,13 @@ for i in range(0,nrows):
     print(fe.submit_to_email(soup,notfound))
     print(-1 if dns == -1 else fe.abnormal_url(domain, url))
     print(-1 if dns == -1 else fe.ssl_final_state(domain,url))
-    '''
     print(fe.i_frame(soup,notfound))
+    print(-1 if dns == -1 else fe.age_of_domain(domain))
+    print(fe.web_traffic(url))
+    #google_index not working due to urllib2 not found
+    print(fe.google_index(url))
+    '''
+    print(fe.statistical_report(url,hostname))
 #ip_address.append(fe.has_ip_address("http://31.220.111.56/asdq12/"))
 #long_url.append(fe.url_length("http://e.webring.com/hub?sid=&amp;ring=hentff98&amp;id=&amp;list"))
 #print(ip_address)
